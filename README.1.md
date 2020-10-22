6e1cbc8b48589443ede54ed2272daf765b71d0de
Qixiang Chen <sherlock.chen@fintelics.com>
2020-10-22 14:35:50 -0400

    second commit

--- README.1.md
+++
@@ -1,2 +0,0 @@
-# Bitspawn-Steam-Connet
-
--- update-ecr.sh
+++
@@ -1,4 +0,0 @@
-$(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t bitspawn-backend-steam .
-docker tag bitspawn-backend-steam:latest :latest
-docker push :latest
